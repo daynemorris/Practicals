7f8b6241ce1cf016f632840cbfe4eb6e9fea3647
daynemorris <71385844+daynemorris@users.noreply.github.com>
2022-09-25 21:51:15 +1000

    Create score.py

---
+++ Prac 2/score.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def main():
+    score = float(input("Enter score: "))
+    outcome = parameter(score)
+    print(outcome)
+    score = randint(0, 100)
+    print(f"Random Score: {score}")
+    outcome = parameter(score)
+    print(outcome)
+
+
+def parameter(score):
+    """Determine users score category"""
+    if 0 < score > 100:
+        return "Invalid score"
+    elif score >= 50:
+        return "Passable"
+    elif score >= 90:
+        return "Excellent"
+    else:
+        return "Bad"
